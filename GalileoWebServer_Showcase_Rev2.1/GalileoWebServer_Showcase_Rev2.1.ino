e07780227dfed611a4a1c6f7161d0cceb1f681bc
BradleyMS <88384460+BradleyMS@users.noreply.github.com>
2023-08-04 10:21:11 -0500

    Update GalileoWebServer_Showcase_Rev2.1.ino
    Organized and commented for clarity.

--- GalileoWebServer_Showcase_Rev2.1/GalileoWebServer_Showcase_Rev2.1.ino
+++ GalileoWebServer_Showcase_Rev2.1/GalileoWebServer_Showcase_Rev2.1.ino
@@ -1,28 +1,33 @@
 /*
-  Web Server
- 
- A simple web server that shows the value of the analog input pins.
- using an Arduino Relay shield. 
- 
- Circuit:
- * Analog inputs attached to pins A0 through A5
- 
+***************************************************************************
+  Web Server for Intel IoT Greenhouse
+ ***************************************************************************
  */
 
+/*
+***************************************************************************
+Include statements for required libraries
+***************************************************************************
+*/
 #include <SPI.h>
-#include <Ethernet.h>
-#include <Servo.h>
+#include <Ethernet.h> // Enables internet access via built-in ethernet port
+#include <Servo.h> // Enables servo controls
+#include "LPD8806.h" // Enable LED controls
 
-/*****************************************************************************/
-/*****************************************************************************/
-#include "LPD8806.h"
-#ifdef __AVR_ATtiny85__
- #include <avr/power.h>
-#endif
-/*****************************************************************************/
-/*****************************************************************************/
+/*
+***************************************************************************
+Intialize global variables
+***************************************************************************
+*/
 
-String version = "Rev2.1.1";
+String version = "Rev2.1.2"; // Update version info to make sure the correct sketch is loaded
+unsigned long currentTime = 0;
+
+/*
+***************************************************************************
+Setup ethernet web server variables
+***************************************************************************
+*/
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
@@ -33,23 +38,18 @@ byte ip[] = { 10, 34, 50, 155 };
 byte gateway[] = { 10, 34, 50, 1 };
 byte subnet[] = { 255, 255, 255, 0 };
 
-// Set up all the timers
-unsigned long currentTime = 0;
-unsigned long previousLEDUpdate = 1000; // Initial time for updating the LED values
-unsigned long previousLEDRefresh = 1000; // Initial time for visually refreshing the LEDs
-unsigned long previousTimeDoor = 1000; // Initial time for door delay
-unsigned long previousTimeLeftWindow = 1000; // Initial time for left window delay
-unsigned long previousTimeRightWindow = 1000; // Initial time for right window delay
-unsigned long previousTimerelay1 = 1000; // Initial time for Relay 1
-unsigned long previousTimerelay2 = 1000; // Initial time for Relay 2
-unsigned long previousTimerelay3 = 1000; // Initial time for Relay 3
-unsigned long previousTimerelay4 = 1000; // Initial time for Relay 4
+// Initialize the Ethernet server library
+// with the IP address and port you want to use 
+// (port 80 is default for HTTP):
+EthernetServer server(80);
 
-/*****************************************************************************/
-/*****************************************************************************/
-// Example to control LPD8806-based RGB LED Modules in a strip
+String controlString; // Captures out URI querystring
 
-/*****************************************************************************/
+/*
+***************************************************************************
+Intialize LED variables
+***************************************************************************
+*/
 
 // Number of RGB LEDs in strand:
 int nLEDs = 26;
@@ -63,31 +63,21 @@ int clockPin = 13;
 // parameters are SPI data and clock pins:
 LPD8806 strip = LPD8806(nLEDs, dataPin, clockPin);
 
-// You can optionally use hardware SPI for faster writes, just leave out
-// the data and clock pin parameters.  But this does limit use to very
-// specific pins on the Arduino.  For "classic" Arduinos (Uno, Duemilanove,
-// etc.), data = pin 11, clock = pin 13.  For Arduino Mega, data = pin 51,
-// clock = pin 52.  For 32u4 Breakout Board+ and Teensy, data = pin B2,
-// clock = pin B1.  For Leonardo, this can ONLY be done on the ICSP pins.
-//LPD8806 strip = LPD8806(nLEDs);
-
 int redLEDValue = 0; // Holds the Red LED value
 int greenLEDValue = 64; // Holds the Green LED value
 int blueLEDValue = 126; // Holds the Blue LED value
-int redLEDUp = 0;
-int greenLEDUp = 0;
-int blueLEDUp = 0;
+int redLEDUp = 0; // determines whether to increase or decrease the Red LED value
+int greenLEDUp = 0; // determines whether to increase or decrease the Green LED value
+int blueLEDUp = 0; // determines whether to increase or decrease the Blue LED value
 
-/*****************************************************************************/
-/*****************************************************************************/
+unsigned long previousLEDUpdate = 1000; // Initial time for updating the LED values
+unsigned long previousLEDRefresh = 1000; // Initial time for visually refreshing the LEDs
 
-
-// Initialize the Ethernet server library
-// with the IP address and port you want to use 
-// (port 80 is default for HTTP):
-EthernetServer server(80);
-
-String controlString; // Captures out URI querystring
+/*
+***************************************************************************
+Intialize servo variables
+***************************************************************************
+*/
 
 // Initialize door & window position variables
 int position_door = 0;
@@ -104,6 +94,16 @@ int servo_door_pin = 6;
 int servo_left_window_pin = 3;
 int servo_right_window_pin = 5;
 
+unsigned long previousTimeDoor = 1000; // Initial time for door delay
+unsigned long previousTimeLeftWindow = 1000; // Initial time for left window delay
+unsigned long previousTimeRightWindow = 1000; // Initial time for right window delay
+
+/*
+***************************************************************************
+Intialize relay variables
+***************************************************************************
+*/
+
 byte relayPin[4] = {2,7,8,10}; // Set relay pin info
 volatile byte relayState[4] = {LOW, LOW, LOW, LOW}; // Initialize relay state variables
 //D2 -> RELAY1
@@ -111,21 +111,25 @@ volatile byte relayState[4] = {LOW, LOW, LOW, LOW}; // Initialize relay state va
 //D8 -> RELAY3
 //D10 -> RELAY4
 
-void setup() {
+unsigned long previousTimerelay1 = 1000; // Initial time for Relay 1
+unsigned long previousTimerelay2 = 1000; // Initial time for Relay 2
+unsigned long previousTimerelay3 = 1000; // Initial time for Relay 3
+unsigned long previousTimerelay4 = 1000; // Initial time for Relay 4
 
-  /*****************************************************************************/
-  /*****************************************************************************/
-  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
-    clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
-  #endif
+/*
+***************************************************************************
+System setup
+***************************************************************************
+*/
+
+void setup() {
 
     // Start up the LED strip
     strip.begin();
 
     // Update the strip, to start they are all 'off'
     strip.show();
-  /*****************************************************************************/
-  /*****************************************************************************/
+
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
 
@@ -139,7 +143,8 @@ void setup() {
   servo_left_window.write(85);
   servo_right_window.write(110);
 
-  for(int i = 0; i < 4; i++)  pinMode(relayPin[i],OUTPUT); // Set relay pins to output
+  // Set all relay pins to output
+  for(int i = 0; i < 4; i++)  pinMode(relayPin[i],OUTPUT);
 
   // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip, gateway, subnet);
@@ -148,19 +153,18 @@ void setup() {
   Serial.println(Ethernet.localIP());
 }
 
+/*
+***************************************************************************
+Main loop
+***************************************************************************
+*/
 
 void loop() {
 
   unsigned long currentTime = millis(); // Universal time reference
   // Serial.println( "Main Time: " + String(currentTime) );
 
-  /*****************************************************************************/
-  /*****************************************************************************/
-
-  ledAttract(currentTime);
-
-  /*****************************************************************************/
-  /*****************************************************************************/
+  ledAttract(currentTime); // This function causes the LEDs to change color every 5 seconds
 
   // Check temperature
   // If high temp open windows and turn on fan
@@ -191,7 +195,7 @@ void loop() {
         // character) and the line is blank, the http request has ended,
         // so you can send a reply
         if (c == '\n' && currentLineIsBlank) {
-          webContent(client);
+          webContent(client); // This function provides the content sent back to a web request
           break;
         }
         if (c == '\n') {
@@ -210,13 +214,18 @@ void loop() {
     client.stop();
     Serial.println("client disonnected");
 
-    // control arduino pin manually
-    manualControls(currentTime);
+    manualControls(currentTime); // This function allows for manual control of servos and relays
 
     //clearing string for next read
     controlString="";
   }
 }
+
+/*
+***************************************************************************
+A Function for the Main Web Page
+***************************************************************************
+*/
 
 void webContent(EthernetClient client) {
   // send a standard http response header
@@ -258,6 +267,12 @@ void webContent(EthernetClient client) {
   client.println("<h2 style=\"color: blue; font-family: arial; text-align: center;\"><a href=\"/?WHITELED\"\">White LEDs</a> - <a href=\"/?REDLED\"\">Red LEDs</a> - <a href=\"/?GREENLED\"\">Green LEDs</a> - <a href=\"/?BLUELED\"\">Blue LEDs</a> - <a href=\"/?LEDOFF\"\">Turn off LEDs</a></h2>");
   client.println("</html>");
 }
+
+/*
+***************************************************************************
+A Function for Manual Controls for Servos and Relays
+***************************************************************************
+*/
 
 void manualControls(unsigned long currentTime) {
     if(controlString.indexOf("?DOOR0") > -1) { close_door(currentTime); } //checks for DOOR0 and closes door
@@ -321,6 +336,12 @@ void manualControls(unsigned long currentTime) {
     if(controlString.indexOf("?LEDOFF") > -1) { colorWipe(strip.Color(   0,   0,   0), 20); Serial.println("LEDs turned off."); } // Off
 }
 
+/*
+***************************************************************************
+A Function to Control the Fan and Windows Based on Temperature
+***************************************************************************
+*/
+
 void tempCheckAutomation(unsigned long currentTime) {
   int tempReading = analogRead(2);
   float tempValue = -0.00277358191992865 * pow(tempReading ,2) + 2.12540388128839 * tempReading + -183.108886107634;
@@ -348,11 +369,23 @@ void tempCheckAutomation(unsigned long currentTime) {
   }
 }
 
+/*
+***************************************************************************
+A Function to Control the Door Based on Movement Detection
+***************************************************************************
+*/
+
 void pirCheckAutomation(unsigned long currentTime) {
   int pirReading = analogRead(3);
   if ((pirReading < 300) && (position_door > 0)) { close_door(currentTime); }
   if ((pirReading > 300) && (position_door < 15)) { open_door(currentTime); }
 }
+
+/*
+***************************************************************************
+A Function to Open the Door
+***************************************************************************
+*/
 
 void open_door(unsigned long currentTime) {
   if ( currentTime - previousTimeDoor > 2000 ) {
@@ -361,12 +394,24 @@ void open_door(unsigned long currentTime) {
   }
 }
 
+/*
+***************************************************************************
+A Function to Close the Door
+***************************************************************************
+*/
+
 void close_door(unsigned long currentTime) {
   if ( currentTime - previousTimeDoor > 2000 ) {
     position_door = 0;
     servo_door.write(135);
   }
 }
+
+/*
+***************************************************************************
+A Function to Open the Left Window
+***************************************************************************
+*/
 
 void open_left_window(unsigned long currentTime) {
   if ( currentTime - previousTimeLeftWindow > 2000 ) {
@@ -375,6 +420,12 @@ void open_left_window(unsigned long currentTime) {
   }
 }
 
+/*
+***************************************************************************
+A Function to Close the Left Window
+***************************************************************************
+*/
+
 void close_left_window(unsigned long currentTime) {
   if ( currentTime - previousTimeLeftWindow > 2000 ) {
     position_left_window = 0;
@@ -382,12 +433,24 @@ void close_left_window(unsigned long currentTime) {
   }
 }
 
+/*
+***************************************************************************
+A Function to Open the Right Window
+***************************************************************************
+*/
+
 void open_right_window(unsigned long currentTime) {
   if ( currentTime - previousTimeRightWindow > 2000 ) {
     position_right_window = 45;
     servo_right_window.write(65);
   }
 }
+
+/*
+***************************************************************************
+A Function to Close the Right Window
+***************************************************************************
+*/
 
 void close_right_window(unsigned long currentTime) {
   if ( currentTime - previousTimeRightWindow > 2000 ) {
@@ -398,6 +461,12 @@ void close_right_window(unsigned long currentTime) {
 
 /*****************************************************************************/
 /*****************************************************************************/
+
+/*
+***************************************************************************
+A Function to Change the LED Color Every 5 Seconds
+***************************************************************************
+*/
 
 void ledAttract(unsigned long currentTime) {
   if ( (currentTime - previousLEDUpdate) > 200 ) {
@@ -548,4 +617,4 @@ uint32_t Wheel(uint16_t WheelPos)
   return(strip.Color(r,g,b));
 }
 /*****************************************************************************/
-/*****************************************************************************/ 
+/*****************************************************************************/
